6d5f0f7a82a5bbf95bfb47eb50aee911fdd6b76b
Paweł Jakubowski <pawel-jakubowski@hotmail.com>
2015-04-02 08:44:39 +0200

    Update build_libs.sh

--- libraries/build_libs.sh
+++ libraries/build_libs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd mathgl-2.3.2
-cmake -D enable-all-widgets=on enable-qt4=on enable-pthread=on .
-cmake -D enable-all-widgets=on enable-qt4=on enable-pthread=on .
+cmake -D enable-all=on -D enable-all-widgets=on -D enable-all-swig=on -D enable-qt4=on .
+cmake -D enable-all=on -D enable-all-widgets=on -D enable-all-swig=on -D enable-qt4=on  .
 make
 
 find . -type f -name 'libmgl*' -exec cp '{}' ../ ';'
